8e82725756ebacc53183ecc2c995ec98f7f43231
Salvador Jimenez <Salvador.Jimenez@ey.com>
2018-09-16 21:35:57 -0500

    Changed table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -19,19 +19,19 @@ const Table = ({ list, pattern, onDismiss }) =>
     <div className="table">
         {list.filter(isSearched(pattern)).map(item =>
             <div key={item.objectID} className="table-row">
-                <span className={midColumn}>
+                <span style={midColumn}>
                     <a href={item.url}>{item.title}</a>
                 </span>
-                <span className={largeColumn}>
+                <span style={largeColumn}>
                     {item.author}
                 </span>
-                <span className={smallColumn}>
+                <span style={smallColumn}>
                     {item.num_comments}
                 </span>
-                <span className={smallColumn}>
+                <span style={smallColumn}>
                     {item.points}
                 </span>
-                <span className={smallColumn}>
+                <span style={smallColumn}>
                     <Button
                         onClick={() => onDismiss(item.objectID)}
                         className="button-inline"
